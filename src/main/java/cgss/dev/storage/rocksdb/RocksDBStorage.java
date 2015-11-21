eda5136df52b97999de140127df1120b5f71f333
CG-SS <criguiss@uol.com.br>
2015-11-21 16:08:07 -0200

    Adding RocksDBStorage javadoc

--- src/main/java/cgss/dev/storage/rocksdb/RocksDBStorage.java
+++ src/main/java/cgss/dev/storage/rocksdb/RocksDBStorage.java
@@ -12,18 +12,42 @@ import org.rocksdb.RocksIterator;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+/**
+ * A KVStorage backed by Rocks DB.
+ * It will save the values on disk inside a RocksDB instance.
+ */
 public class RocksDBStorage implements KVStorage {
 
+    /**
+     * The actual RocksDB instance.
+     */
     private final RocksDB rocksDB;
 
+    /**
+     * Constructor that takes a path where a RocksDB instance will be created.
+     *
+     * @param dbPath The path where the RocksDB instance will be created.
+     * @throws RocksDBException If fails to create a RocksDB instance with the given path.
+     */
     public RocksDBStorage(final String dbPath) throws RocksDBException {
         this(RocksDB.open(new Options().setCreateIfMissing(true), dbPath));
     }
 
+    /**
+     * Constructor that accepts a RocksDB.
+     *
+     * @param rocksDB RocksDB instance to be used.
+     */
     public RocksDBStorage(final RocksDB rocksDB) {
         this.rocksDB = rocksDB;
     }
 
+    /**
+     * Saves the given KeyValue pair.
+     *
+     * @param keyValue Object representing a key value.
+     * @throws KVStorageException If it fails to save the key value pair.
+     */
     @Override
     public void SaveValue(final KeyValue keyValue) throws KVStorageException {
         try {
@@ -33,6 +57,12 @@ public class RocksDBStorage implements KVStorage {
         }
     }
 
+    /**
+     * Deletes a value given the provided key.
+     *
+     * @param key The key for the given value.
+     * @throws KVStorageException If it fails to delete the key value pair.
+     */
     @Override
     public void DeleteValue(final byte[] key) throws KVStorageException {
         try {
@@ -42,6 +72,12 @@ public class RocksDBStorage implements KVStorage {
         }
     }
 
+    /**
+     * Streams the current stored key value pairs.
+     * It uses a RocksDBValueSpliterator to wrap a RocksIterator in order to provide a stream.
+     *
+     * @return A stream containing the stored key value pairs.
+     */
     @Override
     public Stream<KeyValue> StreamValues() {
         final RocksIterator rocksIterator = rocksDB.newIterator();
--- src/main/java/cgss/dev/storage/rocksdb/RocksDBValueSpliterator.java
+++ src/main/java/cgss/dev/storage/rocksdb/RocksDBValueSpliterator.java
@@ -6,13 +6,31 @@ import org.rocksdb.RocksIterator;
 import java.util.Spliterator;
 import java.util.function.Consumer;
 
+/**
+ * A spliterator that wraps a given RocksIterator and provides a spliterator for key values.
+ * Notice that this spliterator is not meant to be used concurrently.
+ */
 class RocksDBValueSpliterator implements Spliterator<KeyValue> {
+    /**
+     * The original iterator.
+     */
     private final RocksIterator rocksIterator;
 
+    /**
+     * Constructor for the RocksDBValueSpliterator.
+     *
+     * @param rocksIterator The original iterator.
+     */
     public RocksDBValueSpliterator(final RocksIterator rocksIterator) {
         this.rocksIterator = rocksIterator;
     }
 
+    /**
+     * Try to continue to consume the original iterator and feed the new value into the stream.
+     *
+     * @param consumer The action.
+     * @return If the spliterator is still valid.
+     */
     @Override
     public boolean tryAdvance(Consumer<? super KeyValue> consumer) {
         if(!rocksIterator.isValid()){
@@ -25,16 +43,31 @@ class RocksDBValueSpliterator implements Spliterator<KeyValue> {
         return rocksIterator.isValid();
     }
 
+    /**
+     * Does nothing, as the original iterator is not splittable.
+     *
+     * @return null
+     */
     @Override
     public Spliterator<KeyValue> trySplit() {
         return null;
     }
 
+    /**
+     * As this is not meant to be concurrent, it returns Long.MAX_VALUE.
+     *
+     * @return Long.MAX_VALUE
+     */
     @Override
     public long estimateSize() {
         return Long.MAX_VALUE;
     }
 
+    /**
+     * Returns the spliterator characteristics.
+     *
+     * @return Spliterator.NONNULL | Spliterator.IMMUTABLE
+     */
     @Override
     public int characteristics() {
         return Spliterator.NONNULL | Spliterator.IMMUTABLE;
